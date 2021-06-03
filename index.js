7884d21f33b5b4132fdfcf957990751d41b90ec1
Graciliana <eagraciliana@gmail.com>
2021-06-02 22:47:39 -0300

    organizando o carregamento dos módulos MVr(Model-View-Router)

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
+const consign = require('consign');
+
 const PORT = 3000;
 
 const app = express();
 
 app.set('json spaces', 4);
 
-app.get("/", (req, res) => res.json({status: "NTASK-API  esta rodando correto"}));
+consign()
 
-app.get('/tasks', (req, res) => {
-    res.json({
-        tasks: [
-            {title: "Fazer compras"},
-            {title: "concertar o pc"},
-        ]
-    });
-} );
 app.listen(PORT, () => console.log(`NTASK-API = porta ${PORT} ok`));
